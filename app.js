6bc7fff6364017034bfe41bc2ca385eeee06ce81
Jake Tripp <shmogens@gmail.com>
2017-09-11 18:36:31 -0500

    DOM functionality almost working

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 // VARIABLES
 // =========
 
+var chain = '';
+
 // ===================
 // AUXILIARY FUNCTIONS
 // ===================
+
+// takes in a string and returns true if equal to +, /, *, -,
+function isOperatorSymbol(x){
+	if ((x === '+') || (x === '/') || (x ==='-') || (x === '*')){
+		return true;
+	} else{
+		return false
+	}
+}
+
 
 // ==============
 // MAIN FUNCTIONS
@@ -12,17 +24,20 @@
 
 // however you store the chain, reset it here
 function reset(){
-	$('label').text('0');
+	chain = '';
+	$('#screen').text('0');
+	$('#chain').text('0');
 }
 
 // remove one thing from the chain
 function backspace(){
-	var text = $('label').text();
+	var text = $('#screen').text();
 	if (text.length === 1) {
-		$('label').text('0');
+		$('#screen').text('0');
 	} else {
 		text = text.substring(0, text.length - 1);
-		$('label').text(text);
+		chain = chain.substring(0, chain.length - 1);
+		$('#screen').text(text);
 	}
 }
 
@@ -36,14 +51,16 @@ function clickEvents(){
 	});
 
 	$('.ui.green.button').on('click', function(e){
-		var labelText = $('label').text();
-		if (labelText === '0'){
-			$('label').text('');
-			labelText = $('label').text();
+		var labelText = $('#screen').text();
+		if (labelText === '0' || isOperatorSymbol(labelText)){
+			$('#screen').text('');
+			labelText = $('#screen').text();
 		}
 		if (labelText.length < 8){
 			var value = $(this).text();
-			$('label').text(labelText += value);			
+			$('#screen').text(labelText += value);
+			chain += value;
+			$('#chain').text(chain);
 		} else {
 			// show semantic (copied type message) saying 'max of 8 digits'
 			// or get users attention somehow?
@@ -51,8 +68,15 @@ function clickEvents(){
 	});
 
 	$('.ui.blue.button').on('click', function(e){
-		$('label').text($(this).text());
+		var value = $(this).text()
+		$('#screen').text(value);
+		chain += value;
+		$('#chain').text(chain);
 	});
+
+	// $('.equals').on('click', function(e){
+	// 	evaluateChain();
+	// })
 }
 
 clickEvents();
